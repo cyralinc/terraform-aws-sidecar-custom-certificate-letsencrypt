c94321b2c1a0f5327c3a7f7cd98bca49d382ec7c
yowgf <alexthomasmol@gmail.com>
2022-03-03 17:14:58 -0300

    Add one word to comment

--- examples/snowflake/main.tf
+++ examples/snowflake/main.tf
@@ -1,6 +1,7 @@
-# To deploy the Sidecar Certificate CA-Signed for a Snowflake sidecar, you don't
-# need to specify `sidecar_subdomains`. Just use `snowflake_account_region`, and
-# the application will use a set of predefined subdomains.
+# To deploy the Sidecar Certificate CA-Signed application for a Snowflake
+# sidecar, you don't need to specify `sidecar_subdomains`. Just use
+# `snowflake_account_region`, and the application will use a set of predefined
+# subdomains.
 module "cyral_sidecar_certificate_casigned" {
   source = "cyralinc/sidecar-certificate-casigned/cyral"
   version = ">= 1.0.0"
