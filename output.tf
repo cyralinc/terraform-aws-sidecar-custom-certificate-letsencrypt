b98f5fcae3fa46e223625825ff8ca673bd7847c2
yowgf <alexthomasmol@gmail.com>
2022-04-25 23:08:41 -0300

    Fix secretID output for cross-account deployments

--- output.tf
+++ output.tf
@@ -1,4 +1,12 @@
+locals {
+  secret_id_output = local.create_certificate_secret ? (
+    aws_secretsmanager_secret.certificate_secret[0].arn
+    ) : (
+    var.sidecar_custom_certificate_secret_arn
+  )
+}
+
 output "secret_id" {
-  value       = aws_secretsmanager_secret.certificate_secret[0].arn
+  value       = local.secret_id_output
   description = "Secret containing the TLS certificate that will be used by the sidecar."
 }
