5186da407b05fabbc5ba00821616b0335c3d7385
Wilson de Carvalho <wjunior@cyral.com>
2022-04-28 02:23:22 -0700

    Variable names following same CFT definitions

--- variables_deployment.tf
+++ variables_deployment.tf
@@ -10,7 +10,7 @@ variable "certificate_manager_s3_bucket" {
   default     = ""
 }
 
-variable "lambda_code_s3_key" {
+variable "certificate_manager_code_s3_key" {
   description = "Object key for the Lambda deployment package on the S3 bucket. LEAVE EMPTY UNLESS YOU WANT TO OVERRIDE THE DEFAULT."
   type        = string
   default     = ""
